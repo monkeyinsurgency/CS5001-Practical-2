cf5cffed89539bda1808573b4bdadc14b7e19b35
Jason Slade <jason@jasonslade.com>
2015-10-16 22:27:19 +0100

    Working nicely, don't fuck it up.

---
+++ src/webServer/Config.java
@@ -0,0 +1,13 @@
+package webServer;
+
+public abstract class Config {
+    public static final String exitString = "exit";
+    public static final int defaultPort = 12345;
+    public static final byte ackByte = 1;
+    public static final String homeDirectory = "./htdocs";
+    public static final String defaultHomepage = "/index.html";
+    public static final String iconsDirectory = "/icons/"; 
+    public static final String logFile = "./practical_errors.txt";
+    public static final String useLog = "./practical_use.txt";
+
+}
